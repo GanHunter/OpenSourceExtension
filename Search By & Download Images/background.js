1123987583e9a6f8a305d9532ce099147d910958
GanYZ <gunhunted@yahoo.com>
2018-06-27 00:34:40 +0800

    Version 1.11a
    Arranging Folders and removing languages

--- Search By & Download Images/background.js
+++ Search By & Download Images/background.js
@@ -1,3 +1,24 @@
+var Provider = class Provider {
+	constructor(name = '', pic = 'pictures/other.png', url = '', chosen = false) {
+		this.name = name;
+		this.pic = pic;
+		this.url = url;
+		this.chosen = chosen;
+	}
+	clone() {
+		return new Provider(this.name, this.pic, this.url, this.chosen);
+	}
+};
+
+const defaultProviders = [
+	new Provider('Google', 'pictures/google.png', 'https://www.google.com/searchbyimage?image_url=%s', true),
+	new Provider('TinEye', 'pictures/tineye.png', 'https://www.tineye.com/search?url=%s'),
+	new Provider('Bing', 'pictures/bing.png', 'https://www.bing.com/images/searchbyimage?FORM=IRSBIQ&cbir=sbi&imgurl=%s'),
+];
+
+function getDefaultProvidersClone() {
+	return defaultProviders.map(p => p.clone());
+}
 
 const MENU_ACTIONS = {
 	PICK_FROM_CURRENT_TAB: {
@@ -32,6 +53,51 @@ browser.runtime.onMessage.addListener((message, sender) => {
 browser.browserAction.onClicked.addListener(tab => {
 	MENU_ACTIONS[pref.get("browserAction")].handler(tab);
 });
+
+// This is  to set up context menu for images, will always get whole storage
+function createContextMenu(storedSettings) {
+	const title = chrome.i18n.getMessage('contextMenuTitle');
+
+	const chosenProviders = storedSettings.storageProviders.filter(p => p.chosen);
+
+	/* When there is only one search provider ticked or selected, it'll not create a submenu of other search providers*/
+	if (chosenProviders.length === 1) {
+		chrome.contextMenus.create({
+			id: chosenProviders[0].name,
+			title,
+			contexts: ['image'],
+		});
+		return;
+	}
+
+	/* Create menu and submenu entries */
+	chrome.contextMenus.create({
+		id: 'Search-By-Image',
+		title,
+		contexts: ['image'],
+	});
+
+	for (const p of chosenProviders) {
+		const contextMenuOptions = {
+			parentId: 'Search-By-Image',
+			id: p.name,
+			title: p.name,
+			contexts: ['image'],
+		};
+		try {
+			chrome.contextMenus.create(contextMenuOptions);
+		} catch (exception) {
+			chrome.contextMenus.create(contextMenuOptions);
+		}
+	}
+	chrome.contextMenus.create({ parentId: 'Search-By-Image', type: 'separator' });
+	chrome.contextMenus.create({
+		parentId: 'Search-By-Image',
+		id: 'openAll',
+		title: chrome.i18n.getMessage('contextMenuOpenAll'),
+		contexts: ['image'],
+	});
+}
 
 pref.ready().then(() => {
 	updateBrowserAction();
@@ -611,74 +677,9 @@ function nestDecodeURIComponent(s) {
 
 
 
-var Provider = class Provider {
-	constructor(name = '', pic = 'pictures/other.png', url = '', chosen = false) {
-		this.name = name;
-		this.pic = pic;
-		this.url = url;
-		this.chosen = chosen;
-	}
-	clone() {
-		return new Provider(this.name, this.pic, this.url, this.chosen);
-	}
-};
 
-const defaultProviders = [
-	new Provider('Google', 'pictures/google.png', 'https://www.google.com/searchbyimage?image_url=%s', true),
-	new Provider('TinEye', 'pictures/tineye.png', 'https://www.tineye.com/search?url=%s'),
-	new Provider('Bing', 'pictures/bing.png', 'https://www.bing.com/images/searchbyimage?FORM=IRSBIQ&cbir=sbi&imgurl=%s'),
-];
 
-function getDefaultProvidersClone() {
-	return defaultProviders.map(p => p.clone());
-}
 
-// This is  to set up context menu for images, will always get whole storage
-function createContextMenu(storedSettings) {
-	const title = chrome.i18n.getMessage('contextMenuTitle');
-
-	const chosenProviders = storedSettings.storageProviders.filter(p => p.chosen);
-
-	/* When there is only one search provider ticked or selected, it'll not create a submenu of other search providers*/
-	if (chosenProviders.length === 1) {
-		chrome.contextMenus.create({
-			id: chosenProviders[0].name,
-			title,
-			contexts: ['image'],
-		});
-		return;
-	}
-
-	
-	
-	/* Create menu and submenu entries */
-	chrome.contextMenus.create({
-		id: 'Search-By-Image',
-		title,
-		contexts: ['image'],
-	});
-
-	for (const p of chosenProviders) {
-		const contextMenuOptions = {
-			parentId: 'Search-By-Image',
-			id: p.name,
-			title: p.name,
-			contexts: ['image'],
-		};
-		try {
-			chrome.contextMenus.create(contextMenuOptions);
-		} catch (exception) {
-			chrome.contextMenus.create(contextMenuOptions);
-		}
-	}
-	chrome.contextMenus.create({ parentId: 'Search-By-Image', type: 'separator' });
-	chrome.contextMenus.create({
-		parentId: 'Search-By-Image',
-		id: 'openAll',
-		title: chrome.i18n.getMessage('contextMenuOpenAll'),
-		contexts: ['image'],
-	});
-}
 
 /* Default settings. If there is nothing in search provider storage, use these values provided. */
 const defaultSettings = {
